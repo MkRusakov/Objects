efbaaf0112932932419153d3fa036b8c28b86428
Mickey <mk.ru5@yandex.ru>
2022-07-24 18:54:55 +0300

    Добавил 2 объекта

---
+++ l1.js
@@ -0,0 +1,22 @@
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false;
+    mileage: 1021,
+    engine: 245,
+    amount: 4.6
+};
+var cadi = {
+    make: "Cadillac",
+    model: "GM",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false;
+    mileage: 12892,
+    engine: 245,
+    amount: 4.6 
+};
